ebadecf0fa7e6f3c70fd8ff583beb61307192570
Rodney <rodney.amani@student.moringaschool.com>
2025-04-10 11:49:10 +0300

    completed

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestComponent() {
+  return <p>This is a test component.</p>;
+}
+
+export default TestComponent;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import TestComponent from './TestComponent';  
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
